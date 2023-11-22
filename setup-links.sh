267b58c393a2aedd2e6f7818175478b82a0598a5
Hayden Rinn <hayden@haydenrinn.com>
2023-11-22 20:19:42 +0000

    setup tpm

--- setup-links.sh
+++ setup-links.sh
@@ -3,6 +3,8 @@
 mkdir -p ~/.config
 
 ln -s ~/.dotfiles/tmux ~/.config/tmux
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -s ~/.dotfiles/helix ~/.config/helix
 
