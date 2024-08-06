5b423daf080b0aa8dceefaefa0e74a0bc9bddd01
Hayden Rinn <haydenrinn@microsoft.com>
2024-08-06 11:58:35 -0700

    Add eat command; and new git functions

--- fish/config.fish
+++ fish/config.fish
@@ -34,9 +34,15 @@ alias gst="git status"
 alias gpf="git push -f"
 alias gp="git push"
 alias gbs="git branch --show-current"
+alias gr="git rev-parse --show-toplevel"
 
 function gpsu
     git push --set-upstream origin (gbs)
+end
+
+function gar
+    # git add repo root
+    git add (gr)
 end
 
 # Zellij aliases
